92f9540dcdf35a65c4675b79fda275c677e649b9
Stan Babourine <stan.babourine@pattern.com>
2025-06-19 14:55:28 -0600

    extending the length of tags columns to 128 to account for sometimes longer than expected names :)

--- assets/databases/heimdall/tables/command_cluster_tags.sql
+++ assets/databases/heimdall/tables/command_cluster_tags.sql
@@ -1,6 +1,8 @@
 create table if not exists command_cluster_tags
 (
     system_command_id int not null,
-    command_cluster_tag varchar(64) not null,
+    command_cluster_tag varchar(128) not null,
     constraint _command_cluster_tags_pk primary key (system_command_id, command_cluster_tag)
 );
+
+alter table command_cluster_tags alter column command_cluster_tag type varchar(128);
--- assets/databases/heimdall/tables/job_cluster_tags.sql
+++ assets/databases/heimdall/tables/job_cluster_tags.sql
@@ -1,7 +1,9 @@
 create table if not exists job_cluster_tags
 (
     system_job_id bigint not null,
-    cluster_tag varchar(64) not null,
+    cluster_tag varchar(128) not null,
     constraint _job_cluster_tags_pk primary key (system_job_id, cluster_tag),
     constraint _job_cluster_tags_cluster_tag unique (cluster_tag, system_job_id)
 );
+
+alter table job_cluster_tags alter column cluster_tag type varchar(128);
