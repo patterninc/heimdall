2e6e1928649de3ce996c60e20b9823d569035b1b
gauravwarale <131575530+gauravwarale@users.noreply.github.com>
2025-06-25 23:25:32 +0530

    Revamp the header logic and fix few error for production build

--- web/src/components/NavigationBar/LeftNavContainer.tsx
+++ web/src/components/NavigationBar/LeftNavContainer.tsx
@@ -1,10 +1,12 @@
 import dynamic from 'next/dynamic'
+import { useUser } from '@/common/hooks/useUser'
 
 const LeftNavBar = dynamic(() => import('./LeftNavBar'), {
   ssr: false,
 })
 
-const LeftNavContainer = ({ user }: { user: string | null }) => {
+const LeftNavContainer = () => {
+  const user = useUser()
   return <LeftNavBar user={user} />
 }
 
