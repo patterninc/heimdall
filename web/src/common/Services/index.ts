b0a58cacc426e344602db4b1d76a619808879d71
gauravwarale <131575530+gauravwarale@users.noreply.github.com>
2025-07-03 21:56:47 +0530

    Show app version with copyright in LeftNav (#17)
    * Nextjs 15 upgrade Heimdall
    
    * PR suggestions
    
    * moving object packages to public pkg to make them available for plugins in other repositories
    
    * support multiple labels
    
    * support for multiple platforms
    
    * enabling web build
    
    * moving context to pkg
    
    * linter
    
    * 23 jun nextjs15 migration (#15)
    
    * moving object.go to make it avaialble for plugins built outside of the public repo
    
    * Fix: Logged-in username not visible in LeftNav (#16)
    
    * delete job working directory after job completes
    
    * pass heimdall version to web ui
    
    * bumping versions
    
    * display version in LeftNav using info icon and tooltip
    
    * Added customize copyright content
    
    * resolved conflict

--- web/src/common/Services/index.ts
+++ web/src/common/Services/index.ts
@@ -40,3 +40,14 @@ export const sortData = <T>(data: T[], sortBy: SortByProps): T[] => {
     return 0
   })
 }
+
+// Utility function to get version
+export const getVersion = () => {
+  if (typeof window === 'undefined') {
+    return null
+  }
+  const value = `; ${document.cookie}`
+  const parts = value.split(`; heimdall-version=`)
+  const version = parts.pop()?.split(';').shift() || null
+  return version ? `${version}` : null
+}
