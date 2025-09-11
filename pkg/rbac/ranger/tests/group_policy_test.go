2ff5b5d8299d67fbebcdf3e7c01bcf60e8f0eba9
ivan.hladush <ivan.hladush@pattern.com>
2025-09-11 10:08:24 -0600

    Add more tests

--- pkg/rbac/ranger/tests/group_policy_test.go
+++ pkg/rbac/ranger/tests/group_policy_test.go
@@ -122,7 +122,98 @@ func TestAllowPermissionsForGroups(t *testing.T) {
 }
 
 func TestDenyPermissionsForGroups(t *testing.T) {
-	tests := []testCase{}
+	tests := []testCase{
+		{
+			name:           "Policy denies select action for group",
+			query:          "SELECT * FROM default_catalog.public.table1",
+			username:       testUserName,
+			expectedResult: false,
+			users:          testDefaultUsers,
+			groups:         testDefaultGroups,
+			policies:       getAllowAllPolicyWithDenyForGroup([]string{"select"}),
+		},
+		{
+			name:           "Policy denies insert action for group, but query is select",
+			query:          "INSERT INTO default_catalog.public.table1 VALUES (1, 'data')",
+			username:       testUserName,
+			expectedResult: false,
+			users:          testDefaultUsers,
+			groups:         testDefaultGroups,
+			policies:       getAllowAllPolicyWithDenyForGroup([]string{"insert"}),
+		},
+		{
+			name:           "Policy denies update action for group but query is select",
+			query:          "SELECT * FROM default_catalog.public.table1",
+			username:       testUserName,
+			expectedResult: true,
+			users:          testDefaultUsers,
+			groups:         testDefaultGroups,
+			policies:       getAllowAllPolicyWithDenyForGroup([]string{"update"}),
+		},
+		{
+			name:           "Policy denies multiple actions including select for group",
+			query:          "SELECT * FROM default_catalog.public.table1",
+			username:       testUserName,
+			expectedResult: false,
+			users:          testDefaultUsers,
+			groups:         testDefaultGroups,
+			policies:       getAllowAllPolicyWithDenyForGroup([]string{"insert", "select", "update"}),
+		},
+		{
+			name:           "Policy denies multiple actions excluding select for group",
+			query:          "SELECT * FROM default_catalog.public.table1",
+			username:       testUserName,
+			expectedResult: true,
+			users:          testDefaultUsers,
+			groups:         testDefaultGroups,
+			policies:       getAllowAllPolicyWithDenyForGroup([]string{"insert", "update", "delete"}),
+		},
+		{
+			name:           "Policy denies select and insert actions for group",
+			query:          "INSERT INTO default_catalog.public.table1 VALUES (1, 'data')",
+			username:       testUserName,
+			expectedResult: true,
+			users:          testDefaultUsers,
+			groups:         testDefaultGroups,
+			policies:       getAllowAllPolicyWithDenyAndExceptionForGroup([]string{"select", "insert"}, []string{"all"}),
+		},
+		{
+			name:           "Policy denies all actions for group but exception allows select",
+			query:          "SELECT * FROM default_catalog.public.table1",
+			username:       testUserName,
+			expectedResult: true,
+			users:          testDefaultUsers,
+			groups:         testDefaultGroups,
+			policies:       getAllowAllPolicyWithDenyAndExceptionForGroup([]string{"all"}, []string{"select"}),
+		},
+		{
+			name:           "Policy denies all actions for group but exception allows insert, but query is select",
+			query:          "SELECT * FROM default_catalog.public.table1",
+			username:       testUserName,
+			expectedResult: false,
+			users:          testDefaultUsers,
+			groups:         testDefaultGroups,
+			policies:       getAllowAllPolicyWithDenyAndExceptionForGroup([]string{"all"}, []string{"insert"}),
+		},
+		{
+			name:           "Policy denies all actions for group but exception allows select and insert, but query is select",
+			query:          "SELECT * FROM default_catalog.public.table1",
+			username:       testUserName,
+			expectedResult: true,
+			users:          testDefaultUsers,
+			groups:         testDefaultGroups,
+			policies:       getAllowAllPolicyWithDenyAndExceptionForGroup([]string{"all"}, []string{"select", "insert"}),
+		},
+		{
+			name:           "Policy denies all actions for group but exception allows select and insert, but query is insert",
+			query:          "INSERT INTO default_catalog.public.table1 VALUES (SELECT * FROM default_catalog.public.table1)",
+			username:       testUserName,
+			expectedResult: true,
+			users:          testDefaultUsers,
+			groups:         testDefaultGroups,
+			policies:       getAllowAllPolicyWithDenyAndExceptionForGroup([]string{"all"}, []string{"insert", "select"}),
+		},
+	}
 
 	runTests(t, tests)
 }
@@ -204,6 +295,112 @@ func getDefaultAllActionsGroupPolicyWithExcludeForDefaultGroup(excludeAccess []s
 					}
 					return accesses
 				}(),
+			},
+		},
+	}
+}
+
+func getAllowAllPolicyWithDenyForGroup(denyAccess []string) []*ranger.Policy {
+	return []*ranger.Policy{
+		{
+			ID:             1,
+			GUID:           "policy-1",
+			IsEnabled:      true,
+			Name:           "Allow select for alice",
+			PolicyType:     0,
+			PolicyPriority: 1,
+			Resources: &ranger.Resource{
+				Catalog: &ranger.ResourceField{
+					Values:     []string{"default_catalog"},
+					IsExcludes: false,
+				},
+				Schema: &ranger.ResourceField{
+					Values:     []string{"public"},
+					IsExcludes: false,
+				},
+				Table: &ranger.ResourceField{
+					Values:     []string{"table1"},
+					IsExcludes: false,
+				},
+			},
+			PolicyItems: []ranger.PolicyItem{
+				{
+					Groups: []string{testGroupName},
+					Accesses: []ranger.Access{
+						{Type: "all"},
+					},
+				},
+			},
+			DenyPolicyItems: []ranger.PolicyItem{
+				{
+					Groups: []string{testGroupName},
+					Accesses: func() []ranger.Access {
+						var accesses []ranger.Access
+						for _, a := range denyAccess {
+							accesses = append(accesses, ranger.Access{Type: a})
+						}
+						return accesses
+					}(),
+				},
+			},
+		},
+	}
+}
+
+func getAllowAllPolicyWithDenyAndExceptionForGroup(denyAccess, exceptionAccess []string) []*ranger.Policy {
+	return []*ranger.Policy{
+		{
+			ID:             1,
+			GUID:           "policy-1",
+			IsEnabled:      true,
+			Name:           "Allow select for alice",
+			PolicyType:     0,
+			PolicyPriority: 1,
+			Resources: &ranger.Resource{
+				Catalog: &ranger.ResourceField{
+					Values:     []string{"default_catalog"},
+					IsExcludes: false,
+				},
+				Schema: &ranger.ResourceField{
+					Values:     []string{"public"},
+					IsExcludes: false,
+				},
+				Table: &ranger.ResourceField{
+					Values:     []string{"table1"},
+					IsExcludes: false,
+				},
+			},
+			PolicyItems: []ranger.PolicyItem{
+				{
+					Groups: []string{testGroupName},
+					Accesses: []ranger.Access{
+						{Type: "all"},
+					},
+				},
+			},
+			DenyPolicyItems: []ranger.PolicyItem{
+				{
+					Groups: []string{testGroupName},
+					Accesses: func() []ranger.Access {
+						var accesses []ranger.Access
+						for _, a := range denyAccess {
+							accesses = append(accesses, ranger.Access{Type: a})
+						}
+						return accesses
+					}(),
+				},
+			},
+			DenyExceptions: []ranger.PolicyItem{
+				{
+					Groups: []string{testGroupName},
+					Accesses: func() []ranger.Access {
+						var accesses []ranger.Access
+						for _, a := range exceptionAccess {
+							accesses = append(accesses, ranger.Access{Type: a})
+						}
+						return accesses
+					}(),
+				},
 			},
 		},
 	}
