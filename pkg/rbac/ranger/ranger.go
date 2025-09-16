8e5aa6fcba68f78995aa02620d65895d0798510c
ivan.hladush <ivan.hladush@pattern.com>
2025-09-16 10:52:38 -0600

    More fixes

--- pkg/rbac/ranger/ranger.go
+++ pkg/rbac/ranger/ranger.go
@@ -2,41 +2,57 @@ package ranger
 
 import (
 	"context"
+	"errors"
 	"log"
 	"strings"
 	"time"
 
+	"gopkg.in/yaml.v3"
+
 	"github.com/patterninc/heimdall/pkg/sql/parser"
+	"github.com/patterninc/heimdall/pkg/sql/parser/factory"
 )
 
-// only private
-// add links
+var (
+	ErrRangerClientConfigIsRequired         = errors.New("ranger client_config is required")
+	ErrRangerParserConfigIsRequired         = errors.New("ranger parser_config is required")
+	ErrRangerParserTypeIsRequired           = errors.New("ranger parser_config.type is required")
+	ErrRangerParserDefaultCatalogIsRequired = errors.New("ranger parser_config.default_catalog is required")
+	ErrRangerUnsupportedParserType          = errors.New("unsupported ranger parser_config.type. supported types: trino")
+)
+
 type Ranger struct {
-	Name                  string                `yaml:"name,omitempty" json:"name,omitempty"`
-	ServiceName           string                `yaml:"service_name,omitempty" json:"service_name,omitempty"`
-	Client                *ClientWrapper        `yaml:"client,omitempty" json:"client,omitempty"`
+	Name                  string `yaml:"name,omitempty" json:"name,omitempty"`
+	ServiceName           string `yaml:"service_name,omitempty" json:"service_name,omitempty"`
+	Client                Client
 	SyncIntervalInMinutes int                   `yaml:"sync_interval_in_minutes,omitempty" json:"sync_interval_in_minutes,omitempty"`
 	AccessReceiver        parser.AccessReceiver `yaml:"parser,omitempty" json:"parser,omitempty"`
-	permissionsByUser     map[string]*UserPermissions
+	permissionsByUser     map[string]*userPermissions
 }
 
-type ParserConfig struct {
+type parserConfig struct {
 	Type           string `yaml:"type,omitempty" json:"type,omitempty"`
 	DefaultCatalog string `yaml:"default_catalog,omitempty" json:"default_catalog,omitempty"`
 }
 
-type UserPermissions struct {
-	AllowPolicies map[parser.Action][]*Policy // todo AllowPolicies
+type clientConfig struct {
+	Endpoint string `yaml:"endpoint,omitempty" json:"endpoint,omitempty"`
+	Username string `yaml:"username,omitempty" json:"username,omitempty"`
+	Password string `yaml:"password,omitempty" json:"password,omitempty"`
+}
+
+type userPermissions struct {
+	AllowPolicies map[parser.Action][]*Policy
 	DenyPolicies  map[parser.Action][]*Policy
 }
 
-func (r *Ranger) Init(ctx context.Context) error {
+func (r *Ranger) Init() error {
 	// first time lets sync state explicitly
 	if err := r.SyncState(); err != nil {
 		return err
 	}
 	go func() {
-		ctx, cancel := context.WithCancel(ctx)
+		ctx, cancel := context.WithCancel(context.Background())
 		defer cancel()
 
 		ticker := time.NewTicker(time.Duration(r.SyncIntervalInMinutes) * time.Minute)
@@ -115,7 +131,7 @@ func (r *Ranger) SyncState() error {
 		}
 	}
 
-	newPermissionsByUser := map[string]*UserPermissions{}
+	newPermissionsByUser := map[string]*userPermissions{}
 	for _, policy := range policies {
 		if !policy.IsEnabled {
 			continue
@@ -133,7 +149,7 @@ func (r *Ranger) SyncState() error {
 		controlledActions := policy.getControlledActions(usersByGroup)
 		for userName, actions := range controlledActions.allowedActionsByUser {
 			if _, ok := newPermissionsByUser[userName]; !ok {
-				newPermissionsByUser[userName] = &UserPermissions{
+				newPermissionsByUser[userName] = &userPermissions{
 					AllowPolicies: map[parser.Action][]*Policy{},
 					DenyPolicies:  map[parser.Action][]*Policy{},
 				}
@@ -144,7 +160,7 @@ func (r *Ranger) SyncState() error {
 		}
 		for userName, actions := range controlledActions.deniedActionsByUser {
 			if _, ok := newPermissionsByUser[userName]; !ok {
-				newPermissionsByUser[userName] = &UserPermissions{
+				newPermissionsByUser[userName] = &userPermissions{
 					AllowPolicies: map[parser.Action][]*Policy{},
 					DenyPolicies:  map[parser.Action][]*Policy{},
 				}
@@ -157,5 +173,49 @@ func (r *Ranger) SyncState() error {
 
 	r.permissionsByUser = newPermissionsByUser
 	log.Println("Syncing users and groups from Apache Ranger for service:", r.ServiceName)
+	return nil
+}
+
+func (r *Ranger) UnmarshalYAML(value *yaml.Node) error {
+	type rawRanger struct {
+		Name                  string        `yaml:"name,omitempty" json:"name,omitempty"`
+		ServiceName           string        `yaml:"service_name,omitempty" json:"service_name,omitempty"`
+		SyncIntervalInMinutes int           `yaml:"sync_interval_in_minutes,omitempty" json:"sync_interval_in_minutes,omitempty"`
+		Client                *clientConfig `yaml:"client"`
+		Parser                *parserConfig `yaml:"parser"`
+	}
+
+	var raw rawRanger
+	if err := value.Decode(&raw); err != nil {
+		return err
+	}
+
+	if raw.Client == nil {
+		return ErrRangerClientConfigIsRequired
+	}
+	if raw.Parser == nil {
+		return ErrRangerParserConfigIsRequired
+	}
+	if raw.Parser.Type == "" {
+		return ErrRangerParserTypeIsRequired
+	}
+	if raw.Parser.DefaultCatalog == "" {
+		return ErrRangerParserDefaultCatalogIsRequired
+	}
+
+	r.Name = raw.Name
+	r.ServiceName = raw.ServiceName
+	r.SyncIntervalInMinutes = raw.SyncIntervalInMinutes
+	r.Client = NewClient(raw.Client.Endpoint, raw.Client.Username, raw.Client.Password)
+
+	accessReceiver, err := factory.CreateParserByType(raw.Parser.Type, raw.Parser.DefaultCatalog)
+	if err != nil {
+		return ErrRangerUnsupportedParserType
+	}
+
+	r.AccessReceiver = accessReceiver
+	if r.SyncIntervalInMinutes == 0 {
+		r.SyncIntervalInMinutes = 5
+	}
 	return nil
 }
