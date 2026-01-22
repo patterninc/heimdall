348ca47eed98924d8383e3edf15aff9a0acc75a1
wlggraham <wlg.graham@gmail.com>
2026-01-22 13:19:41 -0700

    optimize janitor query

--- internal/pkg/janitor/queries/stale_and_canceling_jobs_select.sql
+++ internal/pkg/janitor/queries/stale_and_canceling_jobs_select.sql
@@ -1,27 +1,38 @@
-select
+with candidate_ids as (
+  -- canceling jobs
+  select j.system_job_id
+  from jobs j
+  where j.job_status_id = 7
+
+  union
+
+  -- stale active jobs (drive from active_jobs)
+  select aj.system_job_id
+  from active_jobs aj
+  where aj.last_heartbeat > 0
+    and aj.last_heartbeat < (extract(epoch from now())::int - $1)
+),
+picked as (
+  select
     j.system_job_id,
     j.job_id,
     j.job_status_id,
-    cm.command_id,
-    cl.cluster_id
-from
-    jobs j
-    left join active_jobs aj on j.system_job_id = aj.system_job_id
-    join commands cm on cm.system_command_id = j.job_command_id
-    join clusters cl on cl.system_cluster_id = j.job_cluster_id
-where
-    (
-        -- Stale jobs: must be in active_jobs and have heartbeat timeout
-        (aj.system_job_id is not null and aj.last_heartbeat > 0 and extract(epoch from now())::int - $1 > aj.last_heartbeat)
-        or
-        -- Canceling jobs: status is CANCELING
-        j.job_status_id = 7
-    )
-order by
-    j.system_job_id
-for update of j
-    skip locked
-limit
-    $2
-;
+    j.job_command_id,
+    j.job_cluster_id
+  from jobs j
+  join candidate_ids c on c.system_job_id = j.system_job_id
+  order by j.system_job_id
+  for update of j skip locked
+  limit $2
+)
+select
+  p.system_job_id,
+  p.job_id,
+  p.job_status_id,
+  cm.command_id,
+  cl.cluster_id
+from picked p
+join commands cm on cm.system_command_id = p.job_command_id
+join clusters cl on cl.system_cluster_id = p.job_cluster_id
+order by p.system_job_id;
 
