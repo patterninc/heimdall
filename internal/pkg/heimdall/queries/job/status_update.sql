b412f87c4bc8a2f5a642d1e4d86395a983b135af
wlggraham <wlg.graham@gmail.com>
2026-01-23 09:28:32 -0700

    truncate error if its too long for db constraints

--- internal/pkg/heimdall/queries/job/status_update.sql
+++ internal/pkg/heimdall/queries/job/status_update.sql
@@ -1,7 +1,7 @@
 update jobs
 set
     job_status_id = $1,
-    job_error = $2,
+    job_error = case when $2 is null then null else left($2, 1024) end,
     updated_at = extract(epoch from now())::int
 where
     system_job_id = $3;
